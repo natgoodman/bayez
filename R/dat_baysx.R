dcddb94685e3e90fcc04cabf6f30dab39106d96b
natgoodman <natg@shore.net>
2019-09-11 11:19:28 -0700

    Initial commit of code stubs for baysx

---
+++ R/dat_baysx.R
@@ -0,0 +1,18 @@
+#################################################################################
+##
+## Author:  Nat Goodman
+## Created: 19-09-11
+##          from dat_readme.R created 19-09-11
+##
+## Copyright (C) 2019 Nat Goodman.
+## 
+## Generate data for baysx document.
+##
+## This software is open source, distributed under the MIT License. See LICENSE
+## file at https://github.com/natgoodman/NewPro/FDR/LICENSE 
+##
+#################################################################################
+## ---- Data Generation for README ----
+## STUB obviously
+dat_baysx=function() {}
+
---
+++ R/doc_baysx.R
@@ -0,0 +1,22 @@
+#################################################################################
+##
+## Author:  Nat Goodman
+## Created: 19-09-11
+##          from doc_readme.R created 19-09-11
+##          from misig/doc_readme.R created 19-02-18
+##          from misig/doc_readme.R created 19-02-03 
+##          from misig/doc_siglo.R created 19-01-10
+##          from repwr/R/doc_resig.R created 18-09-05
+## Includes code from repwr/R/docfun_resig.R created 18-10-25
+##
+## Copyright (C) 2019 Nat Goodman.
+## 
+## Generate figures and tables for baysx documemt
+##
+## This software is open source, distributed under the MIT License. See LICENSE
+## file at https://github.com/natgoodman/NewPro/FDR/LICENSE 
+##
+#################################################################################
+## --- Generate Figures and Tables for baysx ---
+## STUB obviously
+doc_baysx=function(sect=NULL) { }
